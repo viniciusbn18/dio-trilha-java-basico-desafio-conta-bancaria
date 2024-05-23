0ec14bbec0833b1687ea2a2f6a12fc2a29495eee
vinicius <vinicius.neves@al.infnet.edu.br>
2024-05-23 18:24:05 -0300

    finalizando o projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -8,6 +8,9 @@ public class ContaTerminal {
         int numeroDaConta;
         double saldo;
 
+        System.out.println("Por favor digite seu nome.");
+        nomeCliente = sc.nextLine();
+
         System.out.println("Por favor, digite o número de sua Agência Bancária, com dígito separado por '-'.");
         agencia = sc.nextLine();
 
@@ -17,5 +20,7 @@ public class ContaTerminal {
         System.out.println("Por favor, digite seu saldo.");
         saldo = sc.nextDouble();
 
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
+                + agencia + " , conta " + numeroDaConta + " e seu saldo de R$ " + saldo + " já está disponível!");
     }
 }
