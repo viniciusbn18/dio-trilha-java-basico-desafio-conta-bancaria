af702ac0540ae5e03a0d71107410fd1da735b28c
vinicius <vinicius.neves@al.infnet.edu.br>
2024-05-23 18:15:07 -0300

    Adicionando as entrada para o usuário digitar as informações

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
         Scanner sc = new Scanner(System.in);
         String agencia, nomeCliente;
         int numeroDaConta;
         double saldo;
+
+        System.out.println("Por favor, digite o número de sua Agência Bancária, com dígito separado por '-'.");
+        agencia = sc.nextLine();
+
+        System.out.println("Porfavor, diigte o número da sua conta.");
+        numeroDaConta = sc.nextInt();
+
+        System.out.println("Por favor, digite seu saldo.");
+        saldo = sc.nextDouble();
 
     }
 }
