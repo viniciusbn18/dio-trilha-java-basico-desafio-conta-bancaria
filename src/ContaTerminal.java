faa957efd51b886b4478fa95e93585daf94a2fab
vinicius <vinicius.neves@al.infnet.edu.br>
2024-05-26 12:01:40 -0300

    Adicionando try-with-resources para garantir que os recursos abertos sejam fechados automaticamente.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,24 +3,27 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
-        String agencia, nomeCliente;
-        int numeroDaConta;
-        double saldo;
+        try (Scanner sc = new Scanner(System.in)) {
 
-        System.out.println("Por favor digite seu nome.");
-        nomeCliente = sc.nextLine();
+            String agencia, nomeCliente;
+            int numeroDaConta;
+            double saldo;
 
-        System.out.println("Por favor, digite o número de sua Agência Bancária, com dígito separado por '-'.");
-        agencia = sc.nextLine();
+            System.out.println("Por favor digite seu nome.");
+            nomeCliente = sc.nextLine();
 
-        System.out.println("Porfavor, diigte o número da sua conta.");
-        numeroDaConta = sc.nextInt();
+            System.out.println("Por favor, digite o número de sua Agência Bancária, com dígito separado por '-'.");
+            agencia = sc.nextLine();
 
-        System.out.println("Por favor, digite seu saldo.");
-        saldo = sc.nextDouble();
+            System.out.println("Porfavor, diigte o número da sua conta.");
+            numeroDaConta = sc.nextInt();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
-                + agencia + " , conta " + numeroDaConta + " e saldo de R$ " + saldo + ", já disponível!");
-    }
+            System.out.println("Por favor, digite seu saldo.");
+            saldo = sc.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
+                    + agencia + " , conta " + numeroDaConta + " e saldo de R$ " + saldo + ", já disponível!");
+        }
+    };
+
 }
